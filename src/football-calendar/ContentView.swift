1bc269f4562bf3220944ab0f7d07ea972ae14b6b
rcorra68 <roberto@xcorra.it>
2024-02-21 16:01:42 +0100

    Inital commit

---
+++ src/football-calendar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  football-calendar
+//
+//  Created by Roberto Corradetti on 21/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ src/football-calendar/football_calendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  football_calendarApp.swift
+//  football-calendar
+//
+//  Created by Roberto Corradetti on 21/02/24.
+//
+
+import SwiftUI
+
+@main
+struct football_calendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
